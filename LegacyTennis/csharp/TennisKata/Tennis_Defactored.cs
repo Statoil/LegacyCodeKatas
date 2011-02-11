541d81cbdf390b142280bcc55cfcdc76242a0d7c
goeran <mail@goeran.no>
2011-02-11 11:21:37 +0100

    added Tennis code. And started to defactore it

---
+++ LegacyTennis/csharp/TennisKata/Tennis_Defactored.cs
@@ -0,0 +1,78 @@
+﻿namespace TennisKata
+{
+    public class TennisDefactored
+    {
+        private int p1, p2;
+
+        public string PrintScore()
+        {
+            bool player1Wins = false;
+            bool player2Wins = false;
+            bool djuice = false;
+            //Keeps track fo player 1 score
+            bool p1add = false;
+            bool p2add = false;
+
+            if (p1 == 3 && p2 == 0)
+            {
+                player1Wins = true;
+            }
+            if (p1 == 0 && p2 == 3)
+                player2Wins = true;
+            if (p1 > 3 && p1 > p2 + 1)
+                player1Wins = true;
+            if (p2 > 3 && p2 > p1 + 1)
+                player2Wins = true;
+
+            // IF A PLAYER HAS NOT WON, THEN SEE IF IT IS JUICE
+            if (p1 > 2 && p2 > 2 && p1 == p2)
+                djuice = true;
+
+            // IT MIGHT BE ADVANTAGE
+            if (p1 > 2 && p2 > 2)
+            {
+                if (p1 == p2 + 1)
+                    return "advantage Player1";
+                if (p2 == p1 + 1)
+                    return "advantage Player2";
+
+                //ONE OF THE PLAYERS MIGHT HAVE WON
+                if (p1 == p2 + 2)
+                    return "Player1 wins";
+                if (p2 == p1 + 2)
+                    return "Player2 wins";
+            }
+
+            // PRINT THE SCORE OUT TO THE USER
+            if (player1Wins)
+                return "Player1 wins";
+            else if (player2Wins)
+                return "Player2 wins";
+            else if (djuice)
+                return "deuce";
+            else if (p1add)
+                return "advantage Player1";
+            else if (p2add)
+                return "advantage Player2";
+            else
+            {
+                var returnValue = string.Format("{0} - {1}", p1, p2);
+                returnValue = returnValue.Replace("0", "love");
+                returnValue = returnValue.Replace("1", "fifteen");
+                returnValue = returnValue.Replace("2", "thirty");
+                returnValue = returnValue.Replace("3", "forty");
+                return returnValue;
+            }
+        }
+
+        public void Player1WinsTheBall()
+        {
+            p1++;
+        }
+
+        public void Player2WinsTheBall()
+        {
+            p2++;
+        }
+    }
+}
---
+++ LegacyTennis/csharp/TennisKata/Tennis_Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TennisKata
+{
+    [TestFixture]
+    public class When_Playing
+    {
+        private TennisDefactored tennis;
+
+        [Test]
+        public void Then_assure_score_is_printed_correctly()
+        {
+            var games = new List<object[]>();
+            games.Add(new object[]{ 0, 0, "love - love" });
+            games.Add(new object[]{ 1, 0, "fifteen - love" });
+            games.Add(new object[]{ 0, 1, "love - fifteen" });
+            games.Add(new object[]{ 1, 1, "fifteen - fifteen" });
+
+            games.Add(new object[]{ 2, 0, "thirty - love" });
+            games.Add(new object[]{ 2, 1, "thirty - fifteen" });
+            games.Add(new object[]{ 2, 2, "thirty - thirty" });
+            games.Add(new object[]{ 1, 2, "fifteen - thirty" });
+            games.Add(new object[]{ 0, 2, "love - thirty" });
+            
+            games.Add(new object[]{ 3, 0, "forty - love" });
+            games.Add(new object[]{ 3, 1, "forty - fifteen" });
+            games.Add(new object[]{ 3, 2, "forty - thirty" });
+            games.Add(new object[]{ 3, 3, "deuce" });
+            games.Add(new object[]{ 2, 3, "thirty - forty" });
+            games.Add(new object[]{ 1, 3, "fifteen - forty" });
+            games.Add(new object[]{ 0, 3, "love - forty" });
+
+            games.Add(new object[]{ 4, 1, "Player1 wins" });
+            games.Add(new object[]{ 4, 2, "Player1 wins" });
+            games.Add(new object[]{ 4, 3, "advantage Player1" });
+            games.Add(new object[]{ 4, 4, "deuce" });
+            games.Add(new object[]{ 3, 4, "advantage Player2" });
+            games.Add(new object[]{ 2, 4, "Player2 wins" });
+            games.Add(new object[]{ 1, 4, "Player2 wins" });
+
+            games.Add(new object[]{ 5, 4, "advantage Player1" });
+            games.Add(new object[]{ 3, 4, "advantage Player2" });
+            games.Add(new object[]{ 4, 5, "advantage Player2" });
+            games.Add(new object[]{ 5, 5, "deuce" });
+            games.Add(new object[]{ 4, 6, "Player2 wins" });
+            games.Add(new object[]{ 6, 4, "Player1 wins" });
+
+
+            foreach (var game in games)
+            {
+                Play((int)game[0], (int)game[1], (string)game[2]);
+            }
+        }
+
+        private void Play(int ballsToPlayer1, int ballsToPlayer2, string expectedScore)
+        {
+            tennis = new TennisDefactored();
+
+            for (int i = 0; i < ballsToPlayer1; i++)
+                tennis.Player1WinsTheBall();
+               
+            for (int i = 0; i < ballsToPlayer2; i++)
+                tennis.Player2WinsTheBall();
+
+            try
+            {
+                Assert.AreEqual(expectedScore, tennis.PrintScore());
+                Console.WriteLine("Successfull game: p1(" + ballsToPlayer1 + ") p2(" + ballsToPlayer2 + ") = " + expectedScore);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
